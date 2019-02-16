11bfb236851062a7fae68a736a3aa9b7dd858ab5
John Doty <john@d0ty.me>
2019-02-16 10:52:43 -0800

    Env vars!

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
+if "%VSCMD_VER%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 cl *.c /D_CRT_SECURE_NO_DEPRECATE /Fe: prettysharp.exe /W4 /O2 /WX
