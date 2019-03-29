8b863f626f64af162ecd40a4b021a0bfc7a1bac6
John Doty <john@d0ty.me>
2019-03-28 20:14:20 -0700

    All editions

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
 if "%VSCMD_VER%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+if "%VSCMD_VER%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+if "%VSCMD_VER%"=="" call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 cl *.c /D_CRT_SECURE_NO_DEPRECATE /Fe: prettysharp.exe /W4 /O2 /WX
